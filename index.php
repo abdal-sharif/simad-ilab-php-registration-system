0a7cadbf1aa21739fd2bf393c7949150b40c864e
unknown <apdulahisherrif@gmail.com>
2022-07-31 13:55:53 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,103 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="short icon" href="/img/logo.png">
+    <link rel="stylesheet"
+    href="https://bootswatch.com/5/flatly/bootstrap.min.css"
+  />
+    <title>Simad ilab</title>
+</head>
+<body>
+    <div class="all">
+       <div class="logo">
+        <h3>Simad Inovation Lab</h3>
+       </div>
+       <form action="">
+
+        <input type="text" id="user-name" name='user-name' class="form-control"  placeholder="Email kaaga" />
+<br>
+       <input type="text" id="user-name" name='user-name' class="form-control"  placeholder="number kaaga" />
+     <br>
+       <input type="text" id="user-name" name='user-name' class="form-control"  placeholder="Email kaaga" />
+   <br>
+       <input type="text" id="user-name" name='user-name' class="form-control"  placeholder="Address kaaga" />
+ <br>
+      
+ <div class="form-group">
+      <select class="form-select" id="exampleSelect1" placeholder="jamcadaada">
+        <option>Jamacadada dooro</option>
+        <option>simad uni..</option>
+        <option>mogadishu uni.</option>
+        <option>jamhuriyo uni..</option>
+        <option>city uni...</option>
+      </select>
+    </div>
+      
+ 
+ <div class="section-team">
+       <div class="form-group">
+
+
+
+
+
+          <div class="special">
+
+           <div class="form-group mt-4 programka  ">
+            <label for="">programka</label>
+            <select name="" id="">
+                <option value="">htrg</option>
+                <option value="">tgbhg</option>
+                <option value="">gbhg</option>
+            </select>
+            </div>
+     
+
+          <div class="form-group mt-4 track">
+            <label for="">track</label>
+            <select name="" id="">
+                <option value="">htrg</option>
+                <option value="">tgbhg</option>
+                <option value="">gbhg</option>
+            </select>
+          </div>
+  
+
+          <div class="form-group mt-4 batch">
+            <label for="">batch</label>
+            <select name="" id="">
+                <option value="">htrg</option>
+                <option value="">tgbhg</option>
+                <option value="">gbhg</option>
+            </select>
+          </div>
+
+          <!-- <div class="gender">
+          <div class="form-check">
+        <input class="form-check-input" type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked="">
+        <label class="form-check-label" for="optionsRadios1">
+         Lab
+        </label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+        <label class="form-check-label" for="optionsRadios2">
+          Dhedig
+        </label>
+      </div>
+          </div> -->
+
+
+          </div>
+           </div>
+       </div>
+       </form>
+       <!-- <button class="btn btn-lg btn-primary col-6 mx-auto d-block big-btn" type="submit" name='submit'>
+           guji
+          </button> -->
+    </div>
+</body>
+</html>
---
+++ log.php
@@ -0,0 +1,43 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="short icon" href="/img/logo.png">
+    <link rel="stylesheet"
+    href="https://bootswatch.com/5/flatly/bootstrap.min.css"
+  />
+    <title>log in </title>
+</head>
+<body class="body-login">
+   <div class="main">
+<div class="middle">
+<form action="">
+<div class="container mt-5">
+      <h4 class="display-3 text-center">Simad inovation lab</h4>
+
+      <form id="conatct-form" method="post">
+        <div class="form-group">
+          <label>magaca isticmalaha</label>
+          <input type="text" id="user-name" name='user-name' class="form-control"  placeholder="Email kaaga" />
+        </div>
+
+        <div class="form-group mt-4 ">
+          <label>number sired</label>
+          <input type="password" id="password" name ='password' class="form-control" placeholder="Password kaaga" />
+        </div>
+
+        <div class="d-grid  mt-4">
+          <button class="btn btn-lg btn-primary col-6 mx-auto d-block" type="submit" name='submit'>
+           guji
+          </button>
+        </div>
+      </form>
+    </div>
+</form>
+
+</div>
+   </div> 
+</body>
+</html>
